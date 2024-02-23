4009c2ab894f511c580782981cdae2493138dd7d
arpit-java <itsarpitgupta@gmail.com>
2024-02-23 08:43:04 +0530

    Initial Commit

---
+++ Udder/UdderApp.swift
@@ -0,0 +1,17 @@
+//
+//  UdderApp.swift
+//  Udder
+//
+//  Created by Arpit Kumar on 23/02/24.
+//
+
+import SwiftUI
+
+@main
+struct UdderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
